5d251dfa509d7c303e800dfef68a5ba5a30aee95
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-17 08:30:04 +0530

    added project pages

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import TopNavBar from '../components/TopNavBar'
+import HeaderBrand from '../components/HeaderBrand'
+import NavBar from '../components/NavBar'
+import HeroSection from '../components/HeroSection'
+
+function Home() {
+  return (
+   <>
+   <TopNavBar/>  
+   <NavBar/>
+   <HeroSection/>
+   </>
+  )
+}
+
+export default Home
