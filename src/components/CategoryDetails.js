42cb3bb443e8dc84452ebbe154fece283b2c8d8a
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-18 11:10:57 +0530

    add condition to display those categories only which has products

--- src/components/CategoryDetails.js
+++ src/components/CategoryDetails.js
@@ -7,24 +7,29 @@ function getFilteredProductOfCategory(categoryName,arrProduct){
     return arrFilteredProduct;
 }
 
-function CategoryDetails(props) {
+function CategoryDetails({category, products}) {
 
-    if(props.products.length == 0){
+    if(products.length == 0){
         return(
             <h2>Loading...</h2>
         )
     } else{
+        var arrFilter = getFilteredProductOfCategory(category.name, products)
+        if(arrFilter.length === 0) {
+            return '';
+        }
+
         return (
             <>
                 <div className="col-lg-3 col-md-4 col-sm-6 pb-1">
                     <a className="text-decoration-none" href="">
                         <div className="cat-item d-flex align-items-center mb-4">
                             <div className="overflow-hidden" style={{ width: 100, height: 100 }}>
-                                <img className="img-fluid" src={props.category.url} alt="" />
+                                <img className="img-fluid" src={arrFilter.length !== 0 ? arrFilter[0].thumbnail : ''} alt="" />
                             </div>
                             <div className="flex-fill pl-3">
-                                <h6>{props.category.name}</h6>
-                                <small className="text-body">{getFilteredProductOfCategory(props.category.name, props.products).length !== 0 ? getFilteredProductOfCategory(props.category.name, props.products).length: ''}</small>
+                                <h6>{category.name}</h6>
+                                <small className="text-body">{arrFilter.length !== 0 ? arrFilter.length: ''}</small>
                             </div>
                         </div>
                     </a>
