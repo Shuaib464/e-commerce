db1281d7ff434e1e6b1290b3cb18da677fccd8c3
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-18 10:14:16 +0530

    register store at index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <BrowserRouter>
- <Routes>
-  <Route path='/' element={<Home/>} />
- </Routes>
- </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+        </Routes>
+        </BrowserRouter>
+    </Provider>
 );
