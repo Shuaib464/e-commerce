608bc960090fa7a3cdbfd65e4f1ad6b3820f0092
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-18 10:23:27 +0530

    add displayCategory component on homePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TopNavBar from '../components/TopNavBar'
 import HeaderBrand from '../components/HeaderBrand'
 import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
+import DisplayCategory from '../components/DisplayCategory'
+import { useDispatch } from 'react-redux'
+import { fetchProducts } from '../redux/features/productSlice'
 
 function Home() {
+
+  const dispatch = useDispatch();
+  useEffect(function(){
+      dispatch(fetchProducts());
+  }, [])
+
   return (
    <>
-   <TopNavBar/>  
-   <NavBar/>
-   <HeroSection/>
+      <TopNavBar/>  
+      <NavBar/>
+      <HeroSection/>
+      
+      {/* category section  */}
+      <DisplayCategory />
    </>
   )
 }
