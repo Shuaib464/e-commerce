ba107f2569a21969dbfdac7e33e502e040a5cb1f
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-19 11:30:39 +0530

    connect shopping-cart component on navbar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './redux/store'
+import Cart from './pages/Cart'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-        <Routes>
-        <Route path='/' element={<Home/>} />
-        </Routes>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/shopping-cart' element={<Cart />} />
+            </Routes>
         </BrowserRouter>
     </Provider>
 );
