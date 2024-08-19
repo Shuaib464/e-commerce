4ca06cc097ed56c148e65aa5f6f08e40638350fb
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-19 11:31:21 +0530

    replace anchor with NavLink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Category from './Category'
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { getCartTotalItems } from '../BLL/utility';
 
 function NavBar() {
+  let cartState = useSelector(state => state.cartR);
   return (
     <>
   {/* Navbar Start */}
@@ -13,14 +17,14 @@ function NavBar() {
 
       <div className="col-lg-9">
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
-          <a href="" className="text-decoration-none d-block d-lg-none">
+          <NavLink to="/home" className="text-decoration-none d-block d-lg-none">
             <span className="h1 text-uppercase text-dark bg-light px-2">
               Multi
             </span>
             <span className="h1 text-uppercase text-light bg-primary px-2 ml-n1">
               Shop
             </span>
-          </a>
+          </NavLink>
           <button
             type="button"
             className="navbar-toggler"
@@ -34,9 +38,9 @@ function NavBar() {
             id="navbarCollapse"
           >
             <div className="navbar-nav mr-auto py-0">
-              <a href="index.html" className="nav-item nav-link active">
+              <NavLink to="/home" className="nav-item nav-link active">
                 Home
-              </a>
+              </NavLink>
               <a href="shop.html" className="nav-item nav-link">
                 Shop
               </a>
@@ -52,9 +56,9 @@ function NavBar() {
                   Pages <i className="fa fa-angle-down mt-1" />
                 </a>
                 <div className="dropdown-menu bg-primary rounded-0 border-0 m-0">
-                  <a href="cart.html" className="dropdown-item">
+                  <NavLink to='/shopping-cart' className="dropdown-item">
                     Shopping Cart
-                  </a>
+                  </NavLink>
                   <a href="checkout.html" className="dropdown-item">
                     Checkout
                   </a>
@@ -74,15 +78,15 @@ function NavBar() {
                   0
                 </span>
               </a>
-              <a href="" className="btn px-0 ml-3">
+              <NavLink to="/shopping-cart" className="btn px-0 ml-3">
                 <i className="fas fa-shopping-cart text-primary" />
                 <span
                   className="badge text-secondary border border-secondary rounded-circle"
                   style={{ paddingBottom: 2 }}
                 >
-                  0
+                  {getCartTotalItems(cartState.arrCart)}
                 </span>
-              </a>
+              </NavLink>
             </div>
           </div>
         </nav>
