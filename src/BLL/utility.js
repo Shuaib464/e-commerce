ade9b1a6c5f6f3b67395bf3883ce3932b1475d5a
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-19 11:28:22 +0530

    create getCartTotalItems function

--- src/BLL/utility.js
+++ src/BLL/utility.js
@@ -2,3 +2,12 @@ export function getDiscountedPrice(price, discountPercent){
     var discountPrice = (price - (price * (discountPercent/100)));
     return discountPrice.toFixed(2);
 }
+
+export function getCartTotalItems(arrCart) {
+    let totalItems = 0
+    for (const e of arrCart) {
+        totalItems += e.quantity;
+    }
+
+    return totalItems;
+}
