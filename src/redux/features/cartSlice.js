7082adf94613bdb5cbae75786b063803052614fb
Shuaib464 <shuaibshekh464@gmail.com>
2024-08-19 11:23:28 +0530

    add increaseProduct, decreaseProduct, removeProduct actions

--- src/redux/features/cartSlice.js
+++ src/redux/features/cartSlice.js
@@ -12,9 +12,31 @@ const cartSlice = createSlice({
             } else{
                 state.arrCart.push({product:p, quantity:1})
             }
+        },
+        increaseProduct: (state, action) => {
+            var p = state.arrCart.find(e => e.product.id === action.payload.id)
+            p.quantity += 1;
+        },
+        decreaseProduct: (state, action) => {
+            var p = state.arrCart.find(e => e.product.id === action.payload.id)
+            if(p.quantity == 1){
+                state.arrCart = state.arrCart.filter(e => e.product.id !== p.product.id)
+            } else {
+                p.quantity -= 1;
+            }
+        },
+        removeProduct: (state, action) => {
+            let product = action.payload;
+            //state.arrCart = state.arrCart.filter(e => e.product.id !== product.id)      // using filter
+
+            // using splice method
+            let index = state.arrCart.findIndex((e) => e.product.id == product.id)
+            if(index !== -1){
+                state.arrCart.splice(index, 1);
+            }
         }
     }
 })
 
 export default cartSlice.reducer
-export const {addToCart} = cartSlice.actions;
+export const {addToCart, increaseProduct, decreaseProduct, removeProduct} = cartSlice.actions;
